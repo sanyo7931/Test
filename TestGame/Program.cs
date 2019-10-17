809e5a97227366350f21f7ed927a091bb027c89b
sanyo7931 <sanyo7931@yahoo.com.tw>
2019-10-17 20:14:00 +0800

    測試

---
+++ TestGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = -1;
+            int gyess;
+            gyess = new Random().Next(100);
+
+            Console.WriteLine("輸入數字: ");
+            while(true)
+            {
+                string input_str = Console.ReadLine();
+                input = int.Parse(input_str);
+
+                if(input==gyess)
+                {
+                    Console.WriteLine("答對了!");
+                    break;
+                }
+                Console.WriteLine("答錯了");
+            }
+        }
+    }
+}
