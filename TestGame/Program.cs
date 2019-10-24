5d64faf9f23d4c0f20ccd499bf196ef712efa992
測試 <sanyo7931@yahoo.com.tw>
2019-10-24 19:28:51 +0800

    測試

--- TestGame/Program.cs
+++ TestGame/Program.cs
@@ -4,17 +4,42 @@ namespace TestGame
 {
     class Program
     {
+        public static int GetInput(int min,int max)
+        {
+            int result = -1;
+            while(true)
+            {
+                string Input_str = Console.ReadLine();
+                if(int.TryParse(Input_str,out result))
+                {
+                    result = int.Parse(Input_str);
+                    if (result >= min && result <= max)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("輸入錯誤!!請輸入(1~100)");
+                }
+                else
+                {
+                    Console.WriteLine("請輸入數字");
+                }
+ 
+            }
+            return result;
+        }
+
+
+
         static void Main(string[] args)
         {
             int input = -1;
-            int gyess;
+            int gyess = 55;
             gyess = new Random().Next(100);
 
             Console.WriteLine("輸入數字: ");
             while(true)
             {
-                string input_str = Console.ReadLine();
-                input = int.Parse(input_str);
+                input = GetInput(1,100);
 
                 if(input==gyess)
                 {
