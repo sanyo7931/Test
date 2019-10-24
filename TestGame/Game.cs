d9b4d074fad127bf7ed2952ed24289e2569b6dbe
測試 <sanyo7931@yahoo.com.tw>
2019-10-24 20:02:35 +0800

    測試

--- TestGame/Game.cs
+++ TestGame/Game.cs
@@ -4,8 +4,31 @@ using System.Text;
 
 namespace TestGame
 {
+
     public class Game
     {
+        public static int GetInput(int min, int max)
+        {
+            int result = -1;
+            while (true)
+            {
+                string Input_str = Console.ReadLine();
+                if (int.TryParse(Input_str, out result))
+                {
+                    result = int.Parse(Input_str);
+                    if (result >= min && result <= max)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("輸入錯誤!!請輸入(1~100)");
+                }
+                else
+                {
+                    Console.WriteLine("請輸入數字");
+                }
+            }
+            return result;
+        }
         public Game()
         {
             Console.WriteLine("發生建構");
@@ -13,6 +36,22 @@ namespace TestGame
         public void Play()
         {
             Console.WriteLine("開始遊戲!!");
+            int input = -1;
+            int gyess = 55;
+            gyess = new Random().Next(100);
+            Console.WriteLine("輸入數字: ");
+            while (true)
+            {
+
+                input = GetInput(1, 100);
+
+                if (input == gyess)
+                {
+                    Console.WriteLine("答對了!");
+                    break;
+                }
+                Console.WriteLine("答錯了");
+            }
         }
     }
 }
--- TestGame/Program.cs
+++ TestGame/Program.cs
@@ -4,51 +4,10 @@ namespace TestGame
 {
     class Program
     {
-        public static int GetInput(int min,int max)
-        {
-            int result = -1;
-            while (true)
-            {
-                string Input_str = Console.ReadLine();
-                if (int.TryParse(Input_str, out result))
-                {
-                    result = int.Parse(Input_str);
-                    if (result >= min && result <= max)
-                    {
-                        break;
-                    }
-                    Console.WriteLine("輸入錯誤!!請輸入(1~100)");
-                }
-                else
-                {
-                    Console.WriteLine("請輸入數字");
-                }
-                }
-            return result;
-        }
-
-
-
         static void Main(string[] args)
         {
-            int input = -1;
-            int gyess = 55;
-            gyess = new Random().Next(100);
             Game game = new Game();
             game.Play();
-            Console.WriteLine("輸入數字: ");
-            while(true)
-            {
-
-                input = GetInput(1,100);
-
-                if(input==gyess)
-                {
-                    Console.WriteLine("答對了!");
-                    break;
-                }
-                Console.WriteLine("答錯了");
-            }
         }
     }
 }
